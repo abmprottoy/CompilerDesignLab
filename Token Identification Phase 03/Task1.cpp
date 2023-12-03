4827a65d413483251736066ef8d0c25cc6c9bed9
ABM Prottoy <abmlabs@outlook.com>
2023-12-03 10:56:25 +0600

    Add Lab 4

---
+++ Token Identification Phase 03/Task1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_set>
+
+using namespace std;
+
+string getOperatorType(char ch) {
+    const char operators[] = {'+', '-', '*', '/', '=', '%'};
+    for (char op : operators) {
+        if (ch == op) {
+            switch (op) {
+                case '+':
+                    return "Addition";
+                case '-':
+                    return "Subtraction";
+                case '*':
+                    return "Multiplication";
+                case '/':
+                    return "Division";
+                case '=':
+                    return "Assignment";
+                case '%':
+                    return "Modulus";
+            }
+        }
+    }
+    return "false";
+}
+
+
+
+std::string extractFunctionName(std::string& line) {
+    int openingParenthesisPos = line.find('(');
+    bool foundClosingParenthesis = false;
+    bool foundOpeningCurlyBrace = false;
+
+    for (char ch : line) {
+        if (ch == ')') {
+            foundClosingParenthesis = true;
+        } else if (ch == '{') {
+            foundOpeningCurlyBrace = true;
+        }
+    }
+
+
+    if (openingParenthesisPos != string::npos && foundClosingParenthesis && foundOpeningCurlyBrace) {
+
+        int i = openingParenthesisPos;
+        while (i > 0 && std::isalnum(line[i - 1])) {
+            --i;
+        }
+
+
+        return line.substr(i, openingParenthesisPos - i);
+    }
+
+
+    return "";
+}
+
+
+
+
+
+int main(){
+    ifstream inputFile("Program.cpp");
+    string line;
+    unordered_set<string> operatorTypes;
+
+    while(getline(inputFile, line)){
+
+        string functionName = extractFunctionName(line);
+
+    if (!functionName.empty() && functionName != "main") {
+        std::cout << "Function Name: " << functionName << std::endl;
+    }
+
+        for (char ch : line) {
+            string type = getOperatorType(ch);
+            if (type != "false") {
+                operatorTypes.insert(type);
+            }
+        }
+    }
+
+    inputFile.close();
+
+    string functionName = extractFunctionName(line);
+
+    if (!functionName.empty()) {
+        std::cout << "Function Name: " << functionName << std::endl;
+    }
+
+    cout << "Operator Types:" << endl;
+    int count = 1;
+    for (const auto &type : operatorTypes) {
+        cout << "Operator-" << count << ": " << type << endl;
+        count++;
+    }
+
+    std::cout << "Total number of operators: " << operatorTypes.size();
+}
